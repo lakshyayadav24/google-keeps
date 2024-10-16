5f420b281d1d206182d0bfbc8ecba098f144213d
lakshyayadav24 <109066624+lakshyayadav24@users.noreply.github.com>
2024-10-16 11:48:05 +0530

    Update index.js

--- src/components/Archive/index.js
+++ src/components/Archive/index.js
@@ -6,14 +6,23 @@ import UnarchiveIcon from '@mui/icons-material/Unarchive';
 import ArchiveOutlinedIcon from '@mui/icons-material/ArchiveOutlined';
 
 
-function Archive({ archivedNotes, onRestore, onTrash , search}) {
+function Archive({ archivedNotes, onRestore, onTrash , search , isOpen}) {
+
+  const columns = {
+    xs: isOpen ? 1 : 1,
+    sm: isOpen ? 1 : 2,
+    md: isOpen ? 2 : 3,
+    lg: isOpen ? 3 : 4,
+    xl: isOpen ? 4 : 5,
+  };
+
   return (
-    <div className="archive-section">
+    <div className={`archive-section ${isOpen ? 'move-left' : ''}`}>
       {archivedNotes.length === 0 ? (
-        <p className="empty-archive" style={{position:'relative' , left:'500px' , top:"140px" }}><ArchiveOutlinedIcon  style={{  fontSize: '400px' , height:"180px"  , color:'LightGrey' , position:"relative" , left:"-100px" }}/> <br/>No notes in archive</p>
+        <p className="empty-archive" ><ArchiveOutlinedIcon  style={{  fontSize: '400px' , height:"180px"  , color:'LightGrey' , position:"relative" , left:"-100px" }}/> <br/>No notes in archive</p>
       ) : (
-        <Box sx={{ width: 1200, minHeight: 253 }}>
-        <Masonry columns={5} spacing={2}>
+        <Box sx={{width:{xs:'400px' , sm:'580px' , md:'800px' , lg:'1000px' , xl:'1200px'}}}>
+        <Masonry columns={columns} spacing={2}>
   
         {archivedNotes.filter((note) =>{
               return search.toLowerCase() === '' 
@@ -25,8 +34,8 @@ function Archive({ archivedNotes, onRestore, onTrash , search}) {
             <h4>{note.title}</h4>
             <p>{note.content}</p>
             <div className="options">
-              <button onClick={() => onRestore(note.id)} className="option-button"><UnarchiveIcon/></button>
-              <button onClick={() => onTrash(note.id)} className="option-button"><DeleteOutlineIcon/></button>
+              <button onClick={() => onRestore(note.id)} className="option-button"><UnarchiveIcon sx={{position:'relative' , left:'-3px' , top:'2px'}}/></button>
+              <button onClick={() => onTrash(note.id)} className="option-button"><DeleteOutlineIcon sx={{position:'relative' , left:'-2px' , top:'2px'}}/></button>
             </div>
           </div>
         ))}
@@ -38,3 +47,4 @@ function Archive({ archivedNotes, onRestore, onTrash , search}) {
 }
 
 export default Archive;
+
