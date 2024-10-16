39bf085c61868ec2360bfec93d4b11d7f4f52b4a
lakshyayadav24 <109066624+lakshyayadav24@users.noreply.github.com>
2024-10-16 11:53:04 +0530

    Update index.js

--- src/components/Trash/index.js
+++ src/components/Trash/index.js
@@ -5,14 +5,23 @@ import RestoreFromTrashIcon from '@mui/icons-material/RestoreFromTrash';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 
-function Trash({ trashedNotes, onRestore, onDelete ,search}) {
+function Trash({ trashedNotes, onRestore, onDelete ,search , isOpen}) {
+
+  const columns = {
+    xs: isOpen ? 1 : 1,
+    sm: isOpen ? 1 : 2,
+    md: isOpen ? 2 : 3,
+    lg: isOpen ? 3 : 4,
+    xl: isOpen ? 4 : 5,
+  };
+
   return (
-    <div className="trash-section">
+    <div className={`trash-section ${isOpen ? 'move-left' : ''}`}>
       {trashedNotes.length === 0 ? (
-        <p className="empty-trash" style={{position:'relative' , left:'580px' , top:"140px"}}><DeleteOutlinedIcon style={{ fontSize: '400px' , height:"180px"  , color:'LightGrey' , position:"relative" , left:"-100px" }} className="trash-icon"/> <br/> No notes in trash</p>
+        <p className="empty-trash" ><DeleteOutlinedIcon style={{ fontSize: '400px' , height:"180px"  , color:'LightGrey' , position:"relative" , left:"-100px" }} className="trash-icon"/> <br/> No notes in trash</p>
       ) : (
-        <Box sx={{ width: 1200, minHeight: 253 }}>
-      <Masonry columns={5} spacing={2}>
+        <Box sx={{width:{xs:'400px' , sm:'580px' , md:'800px' , lg:'1000px' , xl:'1200px'}}}>
+      <Masonry columns={columns} spacing={2}>
         {trashedNotes.filter((note) =>{
               return search.toLowerCase() === '' 
               ? note 
@@ -23,8 +32,8 @@ function Trash({ trashedNotes, onRestore, onDelete ,search}) {
             <h4>{note.title}</h4>
             <p>{note.content}</p>
             <div className="options">
-              <button onClick={() => onRestore(note.id)} title="Recover" className="option-button"><RestoreFromTrashIcon/></button>
-              <button onClick={() => onDelete(note.id)} title="Permanently Delete"  className="option-button"><DeleteForeverIcon/></button>
+              <button onClick={() => onRestore(note.id)} title="Recover" className="option-button"><RestoreFromTrashIcon sx={{position:'relative' , left:'-2px' , top:'2px'}}/></button>
+              <button onClick={() => onDelete(note.id)} title="Permanently Delete"  className="option-button"><DeleteForeverIcon sx={{position:'relative' , left:'-2px' , top:'2px'}}/></button>
             </div>
           </div>
         ))}
@@ -36,3 +45,4 @@ function Trash({ trashedNotes, onRestore, onDelete ,search}) {
 }
 
 export default Trash;
+
