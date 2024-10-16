1924c558d772d35fe9e4663fb5444bb4a995ff4d
lakshyayadav24 <109066624+lakshyayadav24@users.noreply.github.com>
2024-10-16 11:50:09 +0530

    Update index.js

--- src/components/Notes/index.js
+++ src/components/Notes/index.js
@@ -1,31 +1,27 @@
-import React, { useState , useEffect , useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Box from '@mui/material/Box';
 import Masonry from '@mui/lab/Masonry';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import ArchiveIcon from '@mui/icons-material/Archive';
 import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
 
-
-function Notes({ notes, onAddNote, onArchive, onTrash , search}) {
+function Notes({ notes, onAddNote, onArchive, onTrash, search, isOpen, darkMode, hoveringSidebar }) {
   const [isExpanded, setIsExpanded] = useState(false);
   const [noteTitle, setNoteTitle] = useState('');
   const [noteContent, setNoteContent] = useState('');
-  
   let menuRef = useRef();
 
-  useEffect(() =>{
-    let handler = (e) =>{
-      if(!menuRef.current.contains(e.target)){
+  useEffect(() => {
+    let handler = (e) => {
+      if (!menuRef.current.contains(e.target)) {
         setIsExpanded(false);
       }
     };
-    document.addEventListener("mousedown" ,  handler);
-
-    return() =>{
-      document.removeEventListener("mousedown" , handler);
-    }
-  },[]);
-  
+    document.addEventListener("mousedown", handler);
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  }, []);
 
   const handleAddNote = () => {
     if (noteTitle || noteContent) {
@@ -36,16 +32,23 @@ function Notes({ notes, onAddNote, onArchive, onTrash , search}) {
       });
       setNoteTitle('');
       setNoteContent('');
-      setIsExpanded(false); 
+      setIsExpanded(false);
     }
   };
-  
+
+  const columns = {
+    xs: isOpen ? 1 : 1,
+    sm: isOpen ? 1 : 2,
+    md: isOpen ? 2 : 3,
+    lg: isOpen ? 3 : 4,
+    xl: isOpen ? 4 : 5,
+  };
 
   return (
-    <div className="notes-section" >
-        
-      <div ref={menuRef}
-        className={`note-input ${isExpanded ? 'expanded' : ''}`}
+    <div className={`notes-section ${darkMode ? 'dark' : ''} ${isOpen ? 'isOpen' : ''}`}>
+      <div
+        ref={menuRef}
+        className={`note-input ${isExpanded ? 'expanded' : ''} ${darkMode ? 'dark' : ''}`}
         onClick={() => setIsExpanded(true)}
       >
         {isExpanded && (
@@ -66,38 +69,42 @@ function Notes({ notes, onAddNote, onArchive, onTrash , search}) {
         />
         {isExpanded && (
           <div className="note-actions">
-            
-            <button onClick={handleAddNote} className='Add-button'>Add </button>
+            <button onClick={handleAddNote} className='Add-button'>Add</button>
           </div>
         )}
       </div>
-      <div className='display-notes'>
-      {notes.length === 0 ? (
-        <p className="empty-trash" style={{position:'relative' , left:'450px' , top:"140px"}}><LightbulbOutlinedIcon style={{ fontSize: '400px' , height:"180px"  , color:'LightGrey' , position:"relative" , left:"-60px" }} className="trash-icon"/> <br/>Notes you add appear here</p>
-      ) : (
-        <Box sx={{ width: 1300, minHeight: 253 }}>
-          <Masonry columns={{ xs: 4, sm: 5 }} spacing={2}>
-            {notes.filter((note) =>{
-              return search.toLowerCase() === '' 
-              ? note 
-              : (note.title && note.title.toLowerCase().includes(search)) || 
-              (note.content && note.content.toLowerCase().includes(search));
-            } ).map((note) => (
-              <div className="note" key={note.id}>
-                <h4>{note.title}</h4>
-                <p>{note.content}</p>
-                <div className="options">
-                  <button onClick={() => onArchive(note.id)} title='Archive' className='option-button'><ArchiveIcon/></button>
-                  <button onClick={() => onTrash(note.id)} title='Trash' className='option-button'><DeleteOutlineIcon/></button>
+      
+      <div className={`display-notes ${isOpen ? 'move-left' : ''}`} >
+        {notes.length === 0 ? (
+          <p className="empty-trash">
+            <LightbulbOutlinedIcon style={{ fontSize: '400px', height: "180px", color: 'LightGrey', position: "relative", left: "-60px" }} className="trash-icon" />
+            <br />Notes you add appear here
+          </p>
+        ) : (
+          <Box className="note-box" sx={{ padding: '20px', width: { xs: '400px', sm: '580px', md: '800px', lg: '1000px', xl: '1200px' } }}>
+            <Masonry className='note-masonary' columns={columns} spacing={2}>
+              {notes.filter((note) => {
+                return search.toLowerCase() === ''
+                  ? note
+                  : (note.title && note.title.toLowerCase().includes(search)) ||
+                    (note.content && note.content.toLowerCase().includes(search));
+              }).map((note) => (
+                <div className="note" key={note.id} style={hoveringSidebar ? { pointerEvents: 'none' } : {}}>
+                  <h4>{note.title}</h4>
+                  <p>{note.content}</p>
+                  <div className="options">
+                    <button onClick={() => onArchive(note.id)} title='Archive' className='option-button'><ArchiveIcon sx={{ position: 'relative', left: '-3px', top: '2px' }} /></button>
+                    <button onClick={() => onTrash(note.id)} title='Trash' className='option-button'><DeleteOutlineIcon sx={{ position: 'relative', left: '-3px', top: '2px' }} /></button>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </Masonry>
-        </Box>
-      )}
+              ))}
+            </Masonry>
+          </Box>
+        )}
       </div>
     </div>
-  ); 
+  );
 }
 
 export default Notes;
+
