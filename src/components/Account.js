d961666b5d4310c56af5bda5d14543796a03a56a
lakshyayadav24 <109066624+lakshyayadav24@users.noreply.github.com>
2024-10-16 11:54:23 +0530

    Update Account.js

--- src/components/Account.js
+++ src/components/Account.js
@@ -3,15 +3,14 @@ import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
-import PersonAdd from '@mui/icons-material/PersonAdd';
-import Settings from '@mui/icons-material/Settings';
-import Logout from '@mui/icons-material/Logout';
+import AddIcon from '@mui/icons-material/Add';
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
 
-export default function AccountMenu() {
+
+export default function AccountMenu( darkMode) {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
@@ -20,24 +19,31 @@ export default function AccountMenu() {
   const handleClose = () => {
     setAnchorEl(null);
   };
+
   return (
-    <React.Fragment>
+    <div className="account-layout">
       <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-        
         <Tooltip title="Account settings">
           <IconButton
             onClick={handleClick}
             size="small"
-            sx={{ ml: 2  , position:'relative'  , top:'-160px'}}
+            sx={{ ml: 2, position: 'relative', top: '-160px' }}
             aria-controls={open ? 'account-menu' : undefined}
             aria-haspopup="true"
             aria-expanded={open ? 'true' : undefined}
           >
-            <Avatar sx={{ width: 32, height: 32 ,fontSize:'30px' }}></Avatar>
+            <Avatar sx={{ width: 32, height: 32, fontSize: '30px' }} />
           </IconButton>
         </Tooltip>
       </Box>
       <Menu
+        sx={{
+          height: '800px',
+          width: '500px',
+          position: 'relative',
+          top: '-740px',
+          left: '-20px',
+        }}
         anchorEl={anchorEl}
         id="account-menu"
         open={open}
@@ -74,32 +80,62 @@ export default function AccountMenu() {
         transformOrigin={{ horizontal: 'right', vertical: 'top' }}
         anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       >
-        <MenuItem onClick={handleClose}>
-          <Avatar /> Profile
+        <div className={`account-profile ${darkMode ? 'dark' : ''}`}>
+        <div className='ic'
+          onClick={handleClose}
+          
+        >
+          <AccountCircleRoundedIcon className='account-ic'  sx={{ color:'grey' , position:'relative' , top:'20px' , left:'-20px' , fontSize:'100px'}}/>
+        </div>
+        <h2 className='account-options' style={{ position: 'relative', left: '180px', top: '10px', width:'90px' }}>
+          Hi,User !
+        </h2>
+        <div className='account-manage'
+          onClick={handleClose}
+         
+        >
+          Manage Your Google Account
+        </div>
+        <div
+          className="account-container"
+        >
+          <div className='add-account'
+            onClick={handleClose}
+            sx={{
+              width: '180px',
+              height:'60px',
+              position: 'relative',
+              left: '17px',
+              top: '26px',
+              background: 'white',
+              borderRadius: '30px 0 0 30px',
+
+            }}
+          >
+            <AddIcon sx={{color:'blue' , marginLeft:'20px' ,  marginRight:'10px'}}/> Add account
+          </div>
+          <div className='add-accounts'
+            onClick={handleClose}
+            sx={{
+              width: '180px',
+              height:'60px',
+              position: 'relative',
+              left: '200px',
+              top: '-34px',
+              background: 'white',
+              borderRadius: '0 30px 30px 0',
+            }}
+          >
+            <LogoutRoundedIcon sx={{marginLeft:'20px' , marginRight:'10px'}}/>
+            Logout
+          </div>
+        </div>
+        <MenuItem className='account-options' onClick={handleClose} sx={{ position:'relative' , top:'100px' , display:'flex' , justifyContent:'center' }}>
+          Privacy Policy . Terms of Service
         </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <Avatar /> My account
-        </MenuItem>
-        <Divider />
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <PersonAdd fontSize="small" />
-          </ListItemIcon>
-          Add another account
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <Settings fontSize="small" />
-          </ListItemIcon>
-          Settings
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <Logout fontSize="small" />
-          </ListItemIcon>
-          Logout
-        </MenuItem>
+        </div>
       </Menu>
-    </React.Fragment>
+    </div>
   );
 }
+
