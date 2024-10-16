d2139b039289deb9d7aebf45974b4460072bcc47
lakshyayadav24 <109066624+lakshyayadav24@users.noreply.github.com>
2024-10-16 11:51:23 +0530

    Update index.js

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -4,9 +4,7 @@ import ArchiveOutlinedIcon from '@mui/icons-material/ArchiveOutlined';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import './style.css'; 
 
-const Sidebar = ({ isOpen ,  setActiveComponent }) => {
-
-
+const Sidebar = ({ isOpen ,  setActiveComponent , toggleSidebar , darkMode }) => {
 
   const handleButtonClick = (buttonId) => {
       
@@ -14,60 +12,23 @@ const Sidebar = ({ isOpen ,  setActiveComponent }) => {
 
 }
 
-  
+
+const handleToggleSidebar = () => {
+  toggleSidebar(!isOpen);
+}
+
   return (
-    <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-      <div >
-     
-      <div role="button" 
-     
-      onClick={() => handleButtonClick("notes")}
-      className='component'
-      style={{ 
-        
-        
-           overflow:"hidden" , 
-       width:"250px"  , 
-       cursor:"pointer" ,
-        position:"relative" , 
-        left:"-30px" , 
-        marginTop:'-10px' , 
-        borderRadius:"0 40px 40px 0"}}>
-        <LightbulbOutlinedIcon 
-        className='side-icon '
-        sx={{  
-          display:"inline" ,  
-        position:"relative" , 
-        top:"5px"  , 
-        left:"30px" ,  
-        marginRight:"50px" ,
-        width: '24' , 
-        height: '24' , 
-        borderRadius:"50%" }}> 
-         </LightbulbOutlinedIcon>
-          Notes
-          </div>
-      <div role="button" 
-     
-     onClick={() => handleButtonClick("archive")}
-     className='component'
-     style={{  
-         overflow:"hidden" , width:"250px"   , cursor:"pointer" , position:"relative" , left:"-30px" ,  borderRadius:"40px"}}>
-        <ArchiveOutlinedIcon                      className='side-icon '
-
- 
-  sx={{ 
- display:"inline" ,  position:"relative" , top:"5px" , left:"30px", marginRight:"50px" ,width: '24' , height: '24' ,  borderRadius:'50%'}}></ArchiveOutlinedIcon>Archive</div>
-       <div role="button" 
-       onClick={() => handleButtonClick("trash")}
-
-       className='component'
-       style={{  
-        overflow:"hidden" , width:"250px" ,  cursor:"pointer" , position:"relative" , left:"-30px" , borderRadius:"40px"}}><DeleteOutlineOutlinedIcon        
-        className='side-icon '
-
-        
-       sx={{ display:"inline"  ,  position:"relative" , top:"5px" , left:"30px", marginRight:"50px" ,width: '24' , height: '24' ,  borderRadius:'50%'}}/>Trash</div>
+    <div className={`sidebar ${isOpen ? 'open' : ''} ${darkMode ? 'dark' : ''}`}
+      >
+     <div >
+      <div role="button"  onClick={() => handleButtonClick("notes")}  className='component'  style={componentStyle} >
+        <LightbulbOutlinedIcon className='side-icon ' sx={iconStyle} />  Notes </div>
+      
+      <div role="button"  onClick={() => handleButtonClick("archive")} className='component' style={componentStyle} >
+     <ArchiveOutlinedIcon   className='side-icon ' sx={iconStyle} />  Archive</div>
+      
+    <div role="button"  onClick={() => handleButtonClick("trash")} className='component' style={componentStyle}>
+      <DeleteOutlineOutlinedIcon className='side-icon ' sx={iconStyle} /> Trash</div>
     </div>
     </div>
   );
@@ -75,4 +36,13 @@ const Sidebar = ({ isOpen ,  setActiveComponent }) => {
   
 };
 
+const componentStyle = {
+  overflow: "hidden", width: "250px", cursor: "pointer", position: "relative", left: "-30px", marginTop: '20px', borderRadius: "0 40px 40px 0"
+};
+
+const iconStyle = {
+  display: "inline", position: "relative", top: "5px", left: "30px", marginRight: "50px", width: '24', height: '24', borderRadius: "50%"
+};
+
 export default Sidebar;
+
